27949165ee0377c0454194eeb02910d5d3c3aa5e
Stanislav Kozh <printks@yandex.ru>
2022-09-16 23:01:54 +0300

    👉 Let's fix a small error 👉 Creating a ViewModel 👉 We will get a list of all our models 👉 We will display a list of movies on the screen

--- app/src/main/java/com/codexample/moviesapp/navigation/SetupNavHost.kt
+++ app/src/main/java/com/codexample/moviesapp/navigation/SetupNavHost.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.codexample.moviesapp.MainViewModel
 import com.codexample.moviesapp.screens.MainScreen
 import com.codexample.moviesapp.screens.SplashScreen
 import com.codexample.moviesapp.utils.Constants
@@ -15,16 +16,16 @@ sealed class Screens(val route: String) {
 }
 
 @Composable
-fun SetupNavHost(navController: NavHostController) {
+fun SetupNavHost(navController: NavHostController, viewModel: MainViewModel) {
     NavHost(
         navController = navController,
         startDestination = Screens.Splash.route
     ) {
         composable(route = Screens.Splash.route) {
-            SplashScreen(navController = navController)
+            SplashScreen(navController = navController, viewModel = viewModel)
         }
         composable(route = Screens.Main.route) {
-            MainScreen()
+            MainScreen(navController = navController, viewModel = viewModel)
         }
         composable(route = Screens.Details.route) {
 
